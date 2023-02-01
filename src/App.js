0cb265fd01b6baf1c389ecf72b347b927c452f74
Fernando Gimenez <arcalmet@gmail.com>
2023-02-01 11:04:25 -0300

    Cards agregadas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+
+import CardMui from "./components/CardMui/CardMui";
 import Header from "./components/Header/Header";
 import ItemListConteiner from "./components/ItemListConteiner/ItemListConteiner";
 import Layout from "./components/Layout/Layout";
+
 function App() {
 
 
   return (
     <div className="App">
       <Layout>
-      <Header />
-        <ItemListConteiner usuario="Fer" edad={37}/>
+        <Header />
+        <ItemListConteiner usuario="Fer" edad={37} />
       </Layout>
-    
+      <CardMui title="Teclados" imagen="https://http2.mlstatic.com/D_NQ_NP_876249-MLA51700399267_092022-O.webp" descripcion="Teclados Mecanicos" />
+      <CardMui title="Mouse" imagen="https://i.blogs.es/27c890/raton-gaming/1366_2000.jpg" descripcion="Mouse alto rendimiento" />
+      <CardMui title="Audio" imagen="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2020/11/auriculares-razer-2148203.jpg?itok=A_afjJ7v" descripcion="El mejor sonido al alcance de tu mano" />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import CartWidget from "../CartWidget/CartWidget"
 import Navbar from "../Navbar/Navbar"
 
+
+
 const Header = () =>{
     return (
         <div style={{display:"flex", width:"100%", height:"20vh", justifyContent:"space-between", alignItems:"center", backgroundColor:"steelblue"}}>
-            <h2>Logo</h2>
+            <h2>PeriFeria</h2>
             <Navbar />
             <CartWidget />
         </div>
